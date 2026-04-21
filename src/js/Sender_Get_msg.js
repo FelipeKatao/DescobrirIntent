59a932199d7d0fe642d41485ffe7d740d67dfd6a
FelipeKatao <cataofelipe@gmail.com>
2026-04-20 21:52:41 -0300

    Criação de animação base

--- src/js/Sender_Get_msg.js
+++ src/js/Sender_Get_msg.js
@@ -2,7 +2,23 @@
 var Isprimeiro = true
 
 document.getElementById("send").addEventListener("click", function() {
+    if(Isprimeiro){
+        document.getElementById("welcome").classList.add("bye")
+        let a = setInterval(() => {
+            document.getElementById("welcome").remove()
+            ConstruirMensagem()
+            clearInterval(a)
+        }, 1000);
+        Isprimeiro = false
+    }
+    else{
+        ConstruirMensagem()
+    }
 
+});
+
+
+function ConstruirMensagem(){
     const input = document.getElementById("input").value;
     const msg = document.createElement("div");
     msg.classList.add("msg_sender");
@@ -10,8 +26,7 @@ document.getElementById("send").addEventListener("click", function() {
     document.getElementById("msg_input").appendChild(msg);
     document.getElementById("input").value = "";
     ConstruirMensagemResposta()
-});
-
+}
 
 function ConstruirMensagemResposta(){
     const input = "Testes de mensagem"
